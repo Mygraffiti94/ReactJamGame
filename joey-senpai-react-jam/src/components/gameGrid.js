c4cf6bd709a55efda071b1326bd2b4485bf6a50c
JoePatchett <jtpatchett@gmail.com>
2023-12-26 14:44:50 -0500

    state for player but its more for todo

--- joey-senpai-react-jam/src/components/gameGrid.js
+++ joey-senpai-react-jam/src/components/gameGrid.js
@@ -1,7 +1,31 @@
 import Actor from './actor'
+import React from "react"
 
 function GameGrid() {
     console.log('Console says hi!')
+    const [isPlayerOne, setPlayer] = React.useState("yes")
+    console.log(isPlayerOne)
+
+    function changeCharacter() {
+        setPlayer("no")
+    }
+    
+    function onUpArrowClick() {
+        console.log("Up arrow click")
+        
+    }
+    function onLeftArrowClick() {
+        console.log("Left arrow click")
+        
+    }
+    function onRightArrowClick() {
+        console.log("Right arrow click")
+        
+    }
+    function onBottomArrowClick() {
+        console.log("Bottom arrow click")
+    }
+
     return (
         <>
             <Actor />
@@ -13,30 +37,18 @@ function GameGrid() {
             </div>
             <div>
                 <button onClick={onLeftArrowClick}>←</button>
-                <button></button>
+                <button onClick={changeCharacter}>Change Character</button>
                 <button onClick={onRightArrowClick}>→</button>
             </div>
             <div>
                 <span></span>
-                <button onClick={Actor.onBottomArrowClick}>↓</button>
+                <button onClick={onBottomArrowClick}>↓</button>
                 <span></span>
             </div>
         </>
     );
 }
 
-function onUpArrowClick() {
-    
-}
-function onLeftArrowClick() {
-    
-}
-function onRightArrowClick() {
-    console.log("Sanity check")
-    
-}
-function onBottomArrowClick() {
-    console.log("testing how functions work in Javascript")
-}
+
 
 export default GameGrid;
