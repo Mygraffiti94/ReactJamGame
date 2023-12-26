cf7cfceabeae51973fc0c47919357662c64a9620
Mike <mygraffiti94@gmail.com>
2023-12-26 12:40:39 -0500

    Arrow buttons

--- joey-senpai-react-jam/src/App.js
+++ joey-senpai-react-jam/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GameGrid from './components/gameGrid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React with senpai
         </a>
-      </header>
+      </header> */}
+    <GameGrid></GameGrid>
     </div>
   );
 }
---
+++ joey-senpai-react-jam/src/components/gameGrid.js
@@ -0,0 +1,27 @@
+function GameGrid() {
+    return (
+        <>
+            <h1>I'm a gamer</h1>
+            <div>
+                <span></span>
+                <button>↑</button>
+                <span></span>
+            </div>
+            <div>
+                <button>←</button>
+                <button></button>
+                <button>→</button>
+            </div>
+            <div>
+                <span></span>
+                <button>↓</button>
+                <span></span>
+            </div>
+        </>
+    );
+}
+
+function onUpArrowClick() {
+    
+}
+export default GameGrid;
