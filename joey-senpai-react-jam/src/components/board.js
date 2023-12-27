6325dfe571868f3d3ad37789dcf4ca6ac5d3a86a
JoePatchett <jtpatchett@gmail.com>
2023-12-27 13:57:45 -0500

    I really hope this git commit works

---
+++ joey-senpai-react-jam/src/components/board.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react"
+import GameController from "./controller/gameController"
+// import GameGrid from "./gameGrid"
+
+
+export default function Board() {
+    const [gameState, setGameState] = useState({xCoord: 0, yCoord: 0, actorType: 'blue'});
+    // Merge below states into gameState
+    const [playerOne, setPlayerOne] = useState({xCoord: 0, yCoord: 0, isActive: true})
+    const [playerTwo, setPlayerTwo] = useState({xCoord: 0, yCoord: 0, isActive: false})
+    // TODO Set collision decisions
+
+
+    function changeCharacter() {
+        console.log("Changed Character!");
+    }
+
+    function onUpArrowClick() {
+        if (gameState.yCoord > 2) {
+            setGameState(prevState => ({
+                ...prevState,
+                yCoord: prevState.yCoord - 1
+            }));
+    }
+        console.log("X: " + gameState.xCoord + " Y: " + gameState.yCoord);
+    }
+
+    function onDownArrowClick() {
+        if (gameState.yCoord < 8) {
+        setGameState(prevState => ({
+            ...prevState,
+            yCoord: prevState.yCoord + 1
+        }));
+        }
+        console.log("X: " + gameState.xCoord + " Y: " + gameState.yCoord);
+    }
+    function onRightArrowClick() {
+        if (gameState.xCoord < 8)
+        setGameState(prevState => ({
+            ...prevState,
+            xCoord: prevState.xCoord + 1
+        }));
+        console.log("X: " + gameState.xCoord + " Y: " + gameState.yCoord);
+    }
+    function onLeftArrowClick() {
+        if (gameState.xCoord > 2) {
+        setGameState(prevState => ({
+            ...prevState,
+            xCoord: prevState.xCoord - 1
+        }));
+    }
+        console.log("X: " + gameState.xCoord + " Y: " + gameState.yCoord);
+    }
+
+    return (
+        <div>
+            
+            <GameController 
+                leftClick={onLeftArrowClick}
+                rightClick={onRightArrowClick}
+                upClick={onUpArrowClick}
+                downClick={onDownArrowClick}
+                changeCharacter={changeCharacter}
+            /> 
+        
+        </div>
+    );
+}
+/*             <GameController 
+                leftClick={onLeftArrowClick}
+                rightClick={onRightArrowClick}
+                upClick={onUpArrowClick}
+                downClick={onDownArrowClick}
+                <GameGrid />
+            /> */
--- joey-senpai-react-jam/src/components/controller/gameController.js
+++ joey-senpai-react-jam/src/components/controller/gameController.js
@@ -1,25 +1,18 @@
 import { useState } from "react";
 import { Sprite } from '@pixi/react';
 
-function GameController() {
-    const [gameState, setGameState] = useState({xCoord: 0, yCoord: 0, actorType: 'blue'});
- 
-    function onUpArrowClick() {
-        gameState.yCoord++;
-        console.log("X: " + gameState.xCoord + " Y: " + gameState.yCoord);
-    }
+function GameController(props) {
 
     return (
         <>
             <div>
-                <button onClick={onUpArrowClick}>↑</button>
+                <button onClick={props.upClick}>↑</button>
                 <div></div>
-                <button>←</button>
-                <button> <Sprite image="https://pixijs.io/pixi-react/img/bunny.png"/></button>
-                <button>→</button>
+                <button onClick={props.leftClick}>←</button>
+                <button onClick={props.changeCharacter}> <Sprite image="https://pixijs.io/pixi-react/img/bunny.png"/>Change Character</button>
+                <button onClick={props.rightClick}>→</button>
                 <div></div>
-                <button>↓</button>
-                
+                <button onClick={props.downClick}>↓</button>
             </div>
         </>
     );
