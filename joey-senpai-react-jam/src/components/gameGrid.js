64a68b4481b494fffdf9abd73e6478f6a94c6214
Mike <mygraffiti94@gmail.com>
2023-12-28 10:27:52 -0500

    Updates to test map and colors

--- joey-senpai-react-jam/src/components/gameGrid.js
+++ joey-senpai-react-jam/src/components/gameGrid.js
@@ -30,11 +30,17 @@ function GameGrid(props) {
             <div className="center">
                 <div className="interactable-grid">
                     {gridData.map((item, index) => {
-                        if (item.type === "e_wal") {
-                            return <div className="wall"></div>;
-                        }
-                        else {
-                            return <div className="air"></div>;
+                        switch (item.type) {
+                            case "e_wal":
+                                return <div className="wall"></div>;
+                            case "e_air":
+                                return <div className="air"></div>;
+                            case "e_blu":
+                                return <div className="blue-block"></div>;
+                            case "e_org":
+                                return <div className="orange-block"></div>;
+                            default:
+                                break;
                         }
                     })}
                 <Actor
